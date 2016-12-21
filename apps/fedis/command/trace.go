7d05c5895a37c77382c065b55c7ed075e4026ae4
Funky Gao <funky.gao@gmail.com>
2016-12-21 13:58:41 +0800

    use Ctrl-C to stop trace

--- apps/fedis/command/trace.go
+++ apps/fedis/command/trace.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	golog "log"
+	"os"
+	"os/signal"
 	"strings"
 	"sync"
 
@@ -58,7 +60,7 @@ func (this *Trace) Run(args []string) (exitCode int) {
 		this.Ui.Error(err.Error())
 	}
 
-	this.Ui.Warnf("tracing external view changes...")
+	this.Ui.Warn("tracing external view changes...")
 	if err = spectator.AddExternalViewChangeListener(this.external); err != nil {
 		this.Ui.Error(err.Error())
 	}
@@ -73,7 +75,16 @@ func (this *Trace) Run(args []string) (exitCode int) {
 		this.Ui.Error(err.Error())
 	}
 
-	select {}
+	this.Ui.Info("waiting Ctrl-C...")
+
+	// block until SIGINT and SIGTERM
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	this.Ui.Info("disconnecting...")
+	spectator.Disconnect()
+	this.Ui.Info("bye!")
 
 	return
 }
