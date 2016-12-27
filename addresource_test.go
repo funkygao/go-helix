faa25263a5de09b5830cce8777875f80886c8516
Funky Gao <funky.gao@gmail.com>
2016-12-27 11:20:43 +0800

    fix test case bug

--- addresource_test.go
+++ addresource_test.go
@@ -14,8 +14,8 @@ func TestDefaultAddResourceOption(t *testing.T) {
 
 func TestAddResourceOptionValidate(t *testing.T) {
 	opt := DefaultAddResourceOption(3, StateModelMasterSlave)
-	assert.Equal(t, nil, opt.Validate())
+	assert.Equal(t, true, opt.Valid())
 
 	opt.RebalancerMode = ""
-	assert.Equal(t, ErrInvalidAddResourceOption, opt.Validate())
+	assert.Equal(t, false, opt.Valid())
 }
