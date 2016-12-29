7b4a2a64d7e3141f522bafeb679e74030a96899c
Funky Gao <funky.gao@gmail.com>
2016-12-29 14:10:48 +0800

    refactor redis state transition handler

--- apps/fedis/command/redis/state.go
+++ apps/fedis/command/redis/state.go
@@ -11,30 +11,35 @@ func (r *redisParticipant) StateModel() *helix.StateModel {
 	// register state model before connecting
 	sm := helix.NewStateModel()
 	must(sm.AddTransitions([]helix.Transition{
-		{"MASTER", "SLAVE", func(message *model.Message, ctx *helix.Context) {
-			log.Info(color.Cyan("resource[%s/%s] %s->%s", message.Resource(),
-				message.PartitionName(), message.FromState(), message.ToState()))
-		}},
-
-		{"SLAVE", "MASTER", func(message *model.Message, ctx *helix.Context) {
-			log.Info(color.Green("resource[%s/%s] %s->%s", message.Resource(),
-				message.PartitionName(), message.FromState(), message.ToState()))
-
-			// catch up previous master, enable writes, etc.
-		}},
-
-		{"OFFLINE", "SLAVE", func(message *model.Message, ctx *helix.Context) {
-			log.Info(color.Blue("resource[%s/%s] %s->%s", message.Resource(),
-				message.PartitionName(), message.FromState(), message.ToState()))
-
-			// bootstrap data, setup replication, etc.
-		}},
-
-		{"SLAVE", "OFFLINE", func(message *model.Message, ctx *helix.Context) {
-			log.Info(color.Red("resource[%s/%s] %s->%s", message.Resource(),
-				message.PartitionName(), message.FromState(), message.ToState()))
-		}},
+		{"MASTER", "SLAVE", r.master2slave},
+		{"SLAVE", "MASTER", r.slave2master},
+		{"OFFLINE", "SLAVE", r.offline2slave},
+		{"SLAVE", "OFFLINE", r.slave2offline},
 	}))
 
 	return sm
+}
+
+func (r *redisParticipant) master2slave(message *model.Message, ctx *helix.Context) {
+	log.Info(color.Cyan("resource[%s/%s] %s->%s", message.Resource(),
+		message.PartitionName(), message.FromState(), message.ToState()))
+}
+
+func (r *redisParticipant) slave2master(message *model.Message, ctx *helix.Context) {
+	log.Info(color.Green("resource[%s/%s] %s->%s", message.Resource(),
+		message.PartitionName(), message.FromState(), message.ToState()))
+
+	// catch up previous master, enable writes, etc.
+}
+
+func (r *redisParticipant) offline2slave(message *model.Message, ctx *helix.Context) {
+	log.Info(color.Blue("resource[%s/%s] %s->%s", message.Resource(),
+		message.PartitionName(), message.FromState(), message.ToState()))
+
+	// bootstrap data, setup replication, etc.
+}
+
+func (r *redisParticipant) slave2offline(message *model.Message, ctx *helix.Context) {
+	log.Info(color.Red("resource[%s/%s] %s->%s", message.Resource(),
+		message.PartitionName(), message.FromState(), message.ToState()))
 }
